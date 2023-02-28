e8c7973987a37a6f4aadff37d3092d1d5c0bfb8d
Debora Kwon <95396914+Debora-k@users.noreply.github.com>
2023-02-27 23:48:15 -0700

    finished blog post 1

---
+++ example.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './example.css';
+
+function App() {
+
+    return (
+        <div>
+            <h1>Header</h1>
+            <img 
+                className="photo"
+                src={} 
+                alt="This is a photo"
+                width={}
+                height={}
+                />
+        </div>
+    );
+}
+
+
+
+
+export default App;
+
